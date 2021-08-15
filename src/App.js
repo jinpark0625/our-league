bc65b3ae62c81e0d8f65ebc9de8f8619e925e287
jin park <jinpark0625@gmail.com>
2021-08-15 21:01:16 +0900

    default setting react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Login_page from "../src/component/login/login_page";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Login_page />
+    </div>
+  );
+}
+
+export default App;
